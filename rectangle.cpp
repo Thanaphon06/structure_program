85708fc6980fffbdb168d47fa06973c665c3ceb4
Thanaphon06 <tatahahaha007@gmail.com>
2024-07-12 13:35:11 +0700

    homework week2

---
+++ rectangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int h , w , sum;
+    cout << "Enter Length : " ;
+    cin >> h;
+    cout << "Enter Widht : ";
+    cin >> w;
+
+    sum = h * w;
+    
+    cout << "\nArea of Rectagle = " << sum ;
+
+    
+
+}
