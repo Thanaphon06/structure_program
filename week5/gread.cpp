897dea8ade634ae161e565f9334f8cf836ed98f5
Thanaphon06 <68720136+Thanaphon06@users.noreply.github.com>
2024-07-30 15:15:58 +0700

    Update gread.cpp

--- week5/gread.cpp
+++ week5/gread.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int score,a;
+    int score,a,
+    homework, assignment , mid ,final, class_room;
+    
     char grade;
 
     cout << "How many time do you want to use?  ";
@@ -10,9 +12,20 @@ int main(){
 
 for (int i = 0; i < a; i++ ){
 
-    cout << "Please Enter your score : ";
-    cin >> score;
     
+    cout << "Please Enter your score homework(15) : ";
+    cin >> homework;
+    cout << "Please Enter your score assignment(15) : ";
+    cin >> assignment;
+    cout << "Please Enter your score class room(10) : ";
+    cin >> class_room;
+    cout << "Please Enter your score mid term(30) : ";
+    cin >> mid;
+    cout << "Please Enter your score final term(30) : ";
+    cin >> final;
+
+    score = homework + assignment + class_room + mid + final;
+
     if (score >= 0 && score <= 100)
     {
     if (score <= 59)
@@ -31,16 +44,14 @@ for (int i = 0; i < a; i++ ){
     {
         grade = 'A';
     }else{
-        cout << "\nerror\n";
+        cout << "\nError\n";
     }
     }else{
         cout << "\nPlease Enter score between 0 - 100\n";
     }
+    cout << "Your score = " << score << endl;
     cout << "Grade : " << grade << endl;
     }
     
-    
-    
-        
     
 }
